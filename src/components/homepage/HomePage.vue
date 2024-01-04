1f690462920f67ddcda3434be8ce39ead7c7551c
故梦无痕 <1482225463@qq.com>
2024-01-04 23:08:54 +0800

    更改了首页 修改了在main.css 里面vue3自带的样式 添加了element plas 顶头菜单

---
+++ src/components/homepage/HomePage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import MenuHead from '../homepage/pagehead/MenuHead.vue';
+</script>
+
+<template>
+  <div>
+    <el-container >
+      <el-header><MenuHead/></el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomePage from '../components/homepage/HomePage.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <HomePage/>
+  </div>
 </template>
+
